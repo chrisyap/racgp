0ee45709bfafaf52598212755b2dc4b6daf97dab
Chris Yap <radiyap@gmail.com>
2017-02-09 15:37:04 +1100

    Clean up css for input fields

--- assets/js/racgp.js
+++ assets/js/racgp.js
@@ -3,14 +3,38 @@ $('.patientDemographics').on('click','patient-header',function(){
   $('.patient-record').slideToggle();
 });
 
-$('.btn-toggle').on('click',function(){
-  var parent = $(this).parent();
-  if ($(this).hasClass('active')) {
-    $(this).removeClass('active');
-    $(this).find('i').toggle();
-  } else {
-    parent.find('.btn-toggle').removeClass('active');
-    parent.find('.btn-toggle i').toggle();
-    $(this).addClass('active');
+$(".floating-label").each(function () {
+  var group = $(this);
+  var field = group.find('.form-control');
+  if (field.val()){
+    group.addClass('float');
   }
+  field.focus(function () {
+    group.addClass('float');
+    group.addClass('help');
+  }).blur(function() {
+    group.removeClass('help');
+    if (!field.val()) {
+      group.removeClass('float');
+    }
+  })
 });
+
+$('.radio').on('click','.btn',function(){
+  var radio = $(this).parents('.radio');
+  var btn = $(this);
+  radio.find('.btn').removeClass('active');
+  btn.addClass('active');
+})
+
+$('.checkbox').on('click','.btn',function(){
+  var radio = $(this).parents('.checkbox');
+  var btn = $(this);
+  btn.find('i').toggle();
+  if (btn.hasClass('active')){
+    btn.removeClass('active');
+  } else {
+    radio.find('.btn').removeClass('active');
+    btn.addClass('active');
+  }
+})
